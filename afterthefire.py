937154cafab5a3a11fb5f97f1800801165d600d9
merefarley <90054874+merefarley@users.noreply.github.com>
2021-09-26 18:09:21 -0400

    Create afterthefire.py

---
+++ afterthefire.py
@@ -0,0 +1,9 @@
+import random
+pronouns = ["he was walking to", "she was walking to", "they were walking to", "it was walking to", "he was running to", "she was running to", "they were running to","it was running to","he was sprinting to","she was sprinting to", "they were sprinting to","it was sprinting to", "he was crawling to","she was crawling to","they were crawling to","it was crawling to"]
+place = ["the abandoned church", "the bottom of the ocean", "the crematorioum","the haunted house","the asylum","the forest","the woods","the empty mall","the cemetry","the gravesyard"]
+thing = ["the skeleton","the knife","the ghost","the broken mirror", "the key", "the monster", "the figure", "the living dead", "the witch", "the black cat"]
+
+print ("After the fire", random.choice (pronouns))
+print (random.choice(place))
+print ("In hopes of finding", random.choice (thing)) 
+print ("which will hold all the answers")
